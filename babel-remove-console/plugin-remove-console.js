6bebf5d5933b45b7d69c2261907ff3d391257a9b
narumir <openjacoblee@gmail.com>
2020-06-18 03:17:15 +0900

    create custom babel plugin

---
+++ babel-remove-console/plugin-remove-console.js
@@ -0,0 +1,16 @@
+module.exports = function ({ types }) {
+  return {
+    visitor: {
+      ExpressionStatement(path) {
+        if (types.isCallExpression(path.node.expression)) {
+          if (types.isMemberExpression(path.node.expression.callee)) {
+            const memberExp = path.node.expression.callee;
+            if (memberExp.object.name === "console" && memberExp.property.name === "log") {
+              path.remove();
+            }
+          }
+        }
+      }
+    },
+  };
+};
