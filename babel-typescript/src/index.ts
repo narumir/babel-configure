1cca4fb2af2564310aac30a7cc20da4bf56cc11c
narumir <openjacoblee@gmail.com>
2020-06-18 03:10:36 +0900

    babel-typescript add nodemon

--- babel-typescript/src/index.ts
+++ babel-typescript/src/index.ts
@@ -1,11 +1,4 @@
-import "core-js/stable";
-import "regenerator-runtime/runtime";
 import {
   hello,
 } from "src/something";
-import type {
-  Server,
-} from "http";
 hello();
-const server: Server | undefined = undefined;
-console.log(globalThis.length);
